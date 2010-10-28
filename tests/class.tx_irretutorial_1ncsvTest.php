684433a2b850daf778afffcda6cfecbef735e290
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-10-28 19:38:47 +0000

    Tests: Extended unit tests for 1:n CSV
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/irre_tutorial/trunk@39645 735d13b6-9817-0410-8766-e36946ffe9aa

--- tests/class.tx_irretutorial_1ncsvTest.php
+++ tests/class.tx_irretutorial_1ncsvTest.php
@@ -28,12 +28,20 @@
  * @author Oliver Hader <oliver@typo3.org>
  */
 class tx_irretutorial_1ncsvTest extends tx_irretutorial_abstractTest {
+	const TABLE_Hotel = 'tx_irretutorial_1ncsv_hotel';
+	const TABLE_Offer = 'tx_irretutorial_1ncsv_offer';
+	const TABLE_Price = 'tx_irretutorial_1ncsv_price';
+
+	const FIELD_Hotel_Offers = 'offers';
+	const FIELD_Offers_Prices = 'prices';
+
 	/**
 	 * Sets up this test case.
 	 *
 	 * @return void
 	 */
 	public function setUp() {
+		parent::setUp();
 		$this->initializeDatabase();
 		$this->importDataSet($this->getPath() . 'fixtures/data_1ncsv.xml');
 	}
@@ -44,7 +52,25 @@ class tx_irretutorial_1ncsvTest extends tx_irretutorial_abstractTest {
 	 * @return void
 	 */
 	public function tearDown() {
+		parent::tearDown();
 		$this->dropDatabase();
+	}
+
+	/**
+	 * Versionize all children with parent.
+	 *
+	 * @return array
+	 */
+	protected function versionizeAllChildrenWithParent() {
+		$liveElements = array(
+			self::TABLE_Hotel => '1',
+			self::TABLE_Offer => '1,2',
+			self::TABLE_Price => '1,2,3',
+		);
+
+		$this->simulateEditing($liveElements);
+
+		return $liveElements;
 	}
 
 	/**
@@ -52,7 +78,29 @@ class tx_irretutorial_1ncsvTest extends tx_irretutorial_abstractTest {
 	 * @test
 	 */
 	public function areAllChildrenVersonizedWithParent() {
+		$liveElements = $this->versionizeAllChildrenWithParent();
+		$this->assertWorkspaceVersions($liveElements);
 
+		$versionizedHotelId = $this->getWorkpaceVersionId(self::TABLE_Hotel, 1);
+
+			// Workspace:
+		$this->assertChildren(
+			self::TABLE_Hotel, $versionizedHotelId, self::FIELD_Hotel_Offers,
+			array(
+				array(
+					'tableName' => self::TABLE_Offer,
+					't3ver_oid' => 1,
+					't3_origuid' => 1,
+					self::FIELD_Offers_Prices => $this->getWorkpaceVersionId(self::TABLE_Price, 1) . ',' . $this->getWorkpaceVersionId(self::TABLE_Price, 2),
+				),
+				array(
+					'tableName' => self::TABLE_Offer,
+					't3ver_oid' => 2,
+					't3_origuid' => 2,
+					self::FIELD_Offers_Prices => $this->getWorkpaceVersionId(self::TABLE_Price, 3),
+				),
+			)
+		);
 	}
 
 	/**
@@ -60,54 +108,253 @@ class tx_irretutorial_1ncsvTest extends tx_irretutorial_abstractTest {
 	 * @test
 	 */
 	public function areExistingChildVersionsUsedOnParentVersioning() {
+		$childElements = array(
+			self::TABLE_Offer => '2',
+		);
 
+		$this->simulateEditing($childElements);
+		$this->assertWorkspaceVersions($childElements);
+
+			// Live:
+		$this->assertChildren(
+			self::TABLE_Hotel, 1, self::FIELD_Hotel_Offers,
+			array(
+				array(
+					'tableName' => self::TABLE_Offer,
+					'uid' => 1,
+					't3ver_id' => 0,
+					self::FIELD_Offers_Prices => '1,2',
+				),
+				array(
+					'tableName' => self::TABLE_Offer,
+					'uid' => 2,
+					't3ver_id' => 0,
+					self::FIELD_Offers_Prices => '3',
+				),
+			)
+		);
+
+		$versionizedOfferId = $this->getWorkpaceVersionId(self::TABLE_Offer, 2);
+		$versionizedPriceId = $this->getWorkpaceVersionId(self::TABLE_Price, 3);
+
+		$liveElements = array(
+			self::TABLE_Hotel => '1',
+			self::TABLE_Offer => '1',
+			self::TABLE_Price => '1,2',
+		);
+		$liveElementsToBeVersionized = $liveElements;
+		$liveElementsToBeVersionized[self::TABLE_Offer] .= ',' . $versionizedOfferId;
+		$liveElementsToBeVersionized[self::TABLE_Price] .= ',' . $versionizedPriceId;
+
+		$this->simulateEditing($liveElementsToBeVersionized);
+		$this->assertWorkspaceVersions($liveElements);
+
+		$versionizedHotelId = $this->getWorkpaceVersionId(self::TABLE_Hotel, 1);
+
+			// Workspace:
+		$this->assertChildren(
+			self::TABLE_Hotel, $versionizedHotelId, self::FIELD_Hotel_Offers,
+			array(
+				array(
+					'tableName' => self::TABLE_Offer,
+					't3ver_oid' => 1,
+					't3_origuid' => 1,
+					't3ver_id' => 1,
+					self::FIELD_Offers_Prices => $this->getWorkpaceVersionId(self::TABLE_Price, 1) . ',' . $this->getWorkpaceVersionId(self::TABLE_Price, 2),
+				),
+				array(
+					'tableName' => self::TABLE_Offer,
+					't3ver_oid' => 2,
+					't3_origuid' => 2,
+					't3ver_id' => 1,
+					self::FIELD_Offers_Prices => $versionizedPriceId,
+				),
+			)
+		);
 	}
 
 	/**
 	 * @return void
 	 * @test
 	 */
-	public function isChildPublishedSeparately() {
+	public function isChildPublishedSeparatelyIfParentIsNotVersionized() {
+		$childElements = array(
+			self::TABLE_Offer => '2',
+		);
+		$this->simulateEditing($childElements);
 
+		$versionizedOfferId = $this->getWorkpaceVersionId(self::TABLE_Offer, 2);
+		$versionizedPriceId = $this->getWorkpaceVersionId(self::TABLE_Price, 3);
+
+		$this->simulateCommandByStructure(array(
+			self::TABLE_Price => array(
+				'3' => array(
+					'version' => array(
+						'action' => self::COMMAND_Version_Swap,
+						'swapWith' => $versionizedPriceId,
+					)
+				)
+			),
+			self::TABLE_Offer => array(
+				'2' => array(
+					'version' => array(
+						'action' => self::COMMAND_Version_Swap,
+						'swapWith' => $versionizedOfferId,
+					)
+				)
+			),
+		));
+
+			// Live:
+		$this->assertChildren(
+			self::TABLE_Hotel, 1, self::FIELD_Hotel_Offers,
+			array(
+				array(
+					'tableName' => self::TABLE_Offer,
+					'uid' => 1,
+					't3ver_oid' => 0,
+					't3_origuid' => 0,
+					't3ver_id' => 0,
+					self::FIELD_Offers_Prices => '1,2',
+				),
+				array(
+					'tableName' => self::TABLE_Offer,
+					'uid' => 2,
+					't3ver_oid' => 0,
+					't3_origuid' => 2,
+					't3ver_id' => 1, // it was pubslished
+					't3ver_label' => 'Auto-created for WS #' . self::VALUE_WorkspaceId,
+					self::FIELD_Offers_Prices => '3',
+				),
+			)
+		);
 	}
 
 	/**
 	 * @return void
 	 * @test
 	 */
-	public function areAllChildrenPublishedWithParent() {
+	public function isChildPublishedSeparatelyIfParentIsVersionized() {
+		$this->setExpectedLogEntries(1);
 
+		$this->versionizeAllChildrenWithParent();
+
+		$versionizedHotelId = $this->getWorkpaceVersionId(self::TABLE_Hotel, 1);
+		$versionizedOfferId = $this->getWorkpaceVersionId(self::TABLE_Offer, 1);
+
+		$this->simulateVersionCommand(
+			array(
+				'action' => self::COMMAND_Version_Swap,
+				'swapWith' => $versionizedOfferId,
+			),
+			array(
+				self::TABLE_Offer => '1',
+			)
+		);
+
+		$this->assertContains(
+			'depends on a versionized parent or child record and cannot be swapped/published standalone',
+			$this->getLastLogEntryMessage(),
+			'Expected error was not reported.'
+		);
 	}
 
 	/**
 	 * @return void
 	 * @test
 	 */
-	public function isChildSwappedSeparately() {
+	public function isChildSwappedSeparatelyIfParentIsNotVersionized() {
+		$childElements = array(
+			self::TABLE_Offer => '2',
+		);
+		$this->simulateEditing($childElements);
 
+		$versionizedOfferId = $this->getWorkpaceVersionId(self::TABLE_Offer, 2);
+		$versionizedPriceId = $this->getWorkpaceVersionId(self::TABLE_Price, 3);
+
+		$this->simulateCommandByStructure(array(
+			self::TABLE_Price => array(
+				'3' => array(
+					'version' => array(
+						'action' => self::COMMAND_Version_Swap,
+						'swapWith' => $versionizedPriceId,
+						'swapIntoWS' => 1,
+					)
+				)
+			),
+			self::TABLE_Offer => array(
+				'2' => array(
+					'version' => array(
+						'action' => self::COMMAND_Version_Swap,
+						'swapWith' => $versionizedOfferId,
+						'swapIntoWS' => 1,
+					)
+				)
+			),
+		));
+
+			// Live:
+		$this->assertChildren(
+			self::TABLE_Hotel, 1, self::FIELD_Hotel_Offers,
+			array(
+				array(
+					'tableName' => self::TABLE_Offer,
+					'uid' => 1,
+					't3ver_oid' => 0,
+					't3_origuid' => 0,
+					't3ver_id' => 0,
+					self::FIELD_Offers_Prices => '1,2',
+				),
+				array(
+					'tableName' => self::TABLE_Offer,
+					'uid' => 2,
+					't3ver_oid' => 0,
+					't3_origuid' => 2,
+					't3ver_id' => 1, // it was pubslished
+					't3ver_label' => 'Auto-created for WS #' . self::VALUE_WorkspaceId,
+					self::FIELD_Offers_Prices => '3',
+				),
+			)
+		);
 	}
 
 	/**
 	 * @return void
 	 * @test
 	 */
-	public function areAllChildrenSwappedWithParent() {
+	public function isChildSwappedSeparatelyIfParentIsVersionized() {
+		$this->setExpectedLogEntries(2);
 
-	}
+		$this->versionizeAllChildrenWithParent();
 
-	/**
-	 * @return void
-	 * @test
-	 */
-	public function isChildDoubleSwappingSeparately() {
+		$versionizedOfferId = $this->getWorkpaceVersionId(self::TABLE_Offer, 2);
+		$versionizedPriceId = $this->getWorkpaceVersionId(self::TABLE_Price, 3);
 
-	}
+		$this->simulateCommandByStructure(array(
+			self::TABLE_Price => array(
+				'3' => array(
+					'version' => array(
+						'action' => self::COMMAND_Version_Swap,
+						'swapWith' => $versionizedPriceId,
+						'swapIntoWS' => 1,
+					)
+				)
+			),
+			self::TABLE_Offer => array(
+				'2' => array(
+					'version' => array(
+						'action' => self::COMMAND_Version_Swap,
+						'swapWith' => $versionizedOfferId,
+						'swapIntoWS' => 1,
+					)
+				)
+			),
+		));
 
-	/**
-	 * @return void
-	 * @test
-	 */
-	public function areAllChildrenDoubleSwapping() {
-
+		$this->assertContains(
+			'depends on a versionized parent or child record and cannot be swapped/published standalone',
+			$this->getLastLogEntryMessage(),
+			'Expected error was not reported.'
+		);
 	}
 }
