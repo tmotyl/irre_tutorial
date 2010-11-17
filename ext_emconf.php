7cd7cc242fd71dfe5135cc19d0d4a7d64cd69cf5
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-17 15:53:05 +0000

    Release of version 0.3.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/irre_tutorial/trunk@40357 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "irre_tutorial".
 #
-# Auto generated 13-11-2010 16:34
+# Auto generated 17-11-2010 16:52
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Shows how Inline Relational Record Editing could be used to build a structure of hotel, offers and prices differently. Use the "IRRE Tutorial" module to get a quickstart by installing some IRRE sample data.',
 	'category' => 'example',
 	'shy' => 0,
-	'version' => '0.3.1',
+	'version' => '0.3.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:44:{s:9:"ChangeLog";s:4:"4dce";s:10:"README.txt";s:4:"e002";s:16:"ext_autoload.php";s:4:"38ba";s:12:"ext_icon.gif";s:4:"b4e6";s:20:"ext_tables.1ncsv.php";s:4:"138c";s:19:"ext_tables.1nff.php";s:4:"84ca";s:22:"ext_tables.general.php";s:4:"7785";s:21:"ext_tables.mnasym.php";s:4:"580a";s:21:"ext_tables.mnattr.php";s:4:"a852";s:23:"ext_tables.mnmmasym.php";s:4:"8d8d";s:20:"ext_tables.mnsym.php";s:4:"dd80";s:14:"ext_tables.php";s:4:"ae00";s:14:"ext_tables.sql";s:4:"5e78";s:30:"icon_tx_irretutorial_hotel.gif";s:4:"4ad7";s:40:"icon_tx_irretutorial_hotel_offer_rel.gif";s:4:"d3a6";s:34:"icon_tx_irretutorial_hotel_rel.gif";s:4:"9e5c";s:30:"icon_tx_irretutorial_offer.gif";s:4:"1e24";s:40:"icon_tx_irretutorial_offer_price_rel.gif";s:4:"4025";s:30:"icon_tx_irretutorial_price.gif";s:4:"dc05";s:16:"locallang_db.xml";s:4:"6d1f";s:13:"tca.1ncsv.php";s:4:"f293";s:12:"tca.1nff.php";s:4:"5fea";s:14:"tca.mnasym.php";s:4:"8f61";s:14:"tca.mnattr.php";s:4:"c88b";s:16:"tca.mnmmasym.php";s:4:"9153";s:13:"tca.mnsym.php";s:4:"1d20";s:14:"doc/manual.sxw";s:4:"b30a";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"3f25";s:14:"mod1/index.php";s:4:"5a5c";s:18:"mod1/locallang.xml";s:4:"29c4";s:22:"mod1/locallang_mod.xml";s:4:"3d98";s:19:"mod1/moduleicon.gif";s:4:"b4e6";s:17:"res/T3D__IRRE.t3d";s:4:"030b";s:41:"tests/class.tx_irretutorial_1ncsvTest.php";s:4:"c9b3";s:40:"tests/class.tx_irretutorial_1nffTest.php";s:4:"31d3";s:44:"tests/class.tx_irretutorial_abstractTest.php";s:4:"280d";s:29:"tests/fixtures/data_1ncsv.xml";s:4:"cd5e";s:28:"tests/fixtures/data_1nff.xml";s:4:"96ca";s:30:"tests/fixtures/data_mnasym.xml";s:4:"7209";s:32:"tests/fixtures/data_mnmmasym.xml";s:4:"8da1";s:29:"tests/fixtures/data_mnsym.xml";s:4:"e871";s:29:"tests/fixtures/data_pages.xml";s:4:"87ad";s:37:"tests/fixtures/data_sys_workspace.xml";s:4:"5185";}',
+	'_md5_values_when_last_written' => 'a:44:{s:9:"ChangeLog";s:4:"ee40";s:10:"README.txt";s:4:"e002";s:16:"ext_autoload.php";s:4:"38ba";s:12:"ext_icon.gif";s:4:"b4e6";s:20:"ext_tables.1ncsv.php";s:4:"138c";s:19:"ext_tables.1nff.php";s:4:"84ca";s:22:"ext_tables.general.php";s:4:"7785";s:21:"ext_tables.mnasym.php";s:4:"580a";s:21:"ext_tables.mnattr.php";s:4:"a852";s:23:"ext_tables.mnmmasym.php";s:4:"8d8d";s:20:"ext_tables.mnsym.php";s:4:"dd80";s:14:"ext_tables.php";s:4:"ae00";s:14:"ext_tables.sql";s:4:"5e78";s:30:"icon_tx_irretutorial_hotel.gif";s:4:"4ad7";s:40:"icon_tx_irretutorial_hotel_offer_rel.gif";s:4:"d3a6";s:34:"icon_tx_irretutorial_hotel_rel.gif";s:4:"9e5c";s:30:"icon_tx_irretutorial_offer.gif";s:4:"1e24";s:40:"icon_tx_irretutorial_offer_price_rel.gif";s:4:"4025";s:30:"icon_tx_irretutorial_price.gif";s:4:"dc05";s:16:"locallang_db.xml";s:4:"6d1f";s:13:"tca.1ncsv.php";s:4:"f293";s:12:"tca.1nff.php";s:4:"5fea";s:14:"tca.mnasym.php";s:4:"8f61";s:14:"tca.mnattr.php";s:4:"c88b";s:16:"tca.mnmmasym.php";s:4:"9153";s:13:"tca.mnsym.php";s:4:"1d20";s:14:"doc/manual.sxw";s:4:"b30a";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"3f25";s:14:"mod1/index.php";s:4:"5a5c";s:18:"mod1/locallang.xml";s:4:"29c4";s:22:"mod1/locallang_mod.xml";s:4:"3d98";s:19:"mod1/moduleicon.gif";s:4:"b4e6";s:17:"res/T3D__IRRE.t3d";s:4:"030b";s:41:"tests/class.tx_irretutorial_1ncsvTest.php";s:4:"0538";s:40:"tests/class.tx_irretutorial_1nffTest.php";s:4:"f876";s:44:"tests/class.tx_irretutorial_abstractTest.php";s:4:"6258";s:29:"tests/fixtures/data_1ncsv.xml";s:4:"cd5e";s:28:"tests/fixtures/data_1nff.xml";s:4:"96ca";s:30:"tests/fixtures/data_mnasym.xml";s:4:"7209";s:32:"tests/fixtures/data_mnmmasym.xml";s:4:"8da1";s:29:"tests/fixtures/data_mnsym.xml";s:4:"e871";s:29:"tests/fixtures/data_pages.xml";s:4:"87ad";s:37:"tests/fixtures/data_sys_workspace.xml";s:4:"5185";}',
 	'suggests' => array(
 	),
 );
