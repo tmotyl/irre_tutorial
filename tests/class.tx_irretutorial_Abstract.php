e901c253b0aaee1657d8a22c46da7f75045841a5
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-22 14:47:01 +0000

    --
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/irre_tutorial/trunk@45437 735d13b6-9817-0410-8766-e36946ffe9aa

--- tests/class.tx_irretutorial_Abstract.php
+++ tests/class.tx_irretutorial_Abstract.php
@@ -27,7 +27,7 @@
  *
  * @author Oliver Hader <oliver@typo3.org>
  */
-abstract class tx_irretutorial_abstract extends tx_phpunit_database_testcase {
+abstract class tx_irretutorial_Abstract extends tx_phpunit_database_testcase {
 	const TABLE_Pages = 'pages';
 
 	const VALUE_TimeStamp = 1250000000;
