ccc70e456e8e6618b91d087495c0ab5fb91aa252
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-04 22:23:23 +0000

    [TASK] #45151: Extend test-case for 1n:CSV copy actions on workspaces
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/irre_tutorial/trunk@70768 735d13b6-9817-0410-8766-e36946ffe9aa

--- tests/class.tx_irretutorial_1ncsvWorkspacesTest.php
+++ tests/class.tx_irretutorial_1ncsvWorkspacesTest.php
@@ -165,6 +165,112 @@ class tx_irretutorial_1ncsvWorkspacesTest extends tx_irretutorial_AbstractWorksp
 		);
 	}
 
+	/****************************************************************
+	 * COPY Behaviour
+	 ****************************************************************/
+
+	/**
+	 * @test
+	 */
+	public function liveRecordsAreCopied() {
+		$tceMain = $this->simulateCommand(
+			self::COMMAND_Copy,
+			-1,
+			array(
+				self::TABLE_Hotel => 1
+			)
+		);
+
+		$placeholderHotelId = $tceMain->copyMappingArray_merged[self::TABLE_Hotel][1];
+		$versionizedHotelId = $tceMain->getAutoVersionId(self::TABLE_Hotel, $placeholderHotelId);
+
+		$this->assertGreaterThan($placeholderHotelId, $versionizedHotelId);
+
+		$placeholderOfferId = $tceMain->copyMappingArray_merged[self::TABLE_Offer][2];
+		$placeholderPriceId = $tceMain->copyMappingArray_merged[self::TABLE_Price][3];
+
+		$this->assertGreaterThan(0, $placeholderOfferId, 'Seems like child reference have not been considered');
+		$this->assertGreaterThan(0, $placeholderPriceId, 'Seems like child reference have not been considered');
+
+		$versionizedOfferId = $tceMain->getAutoVersionId(self::TABLE_Offer, $placeholderOfferId);
+		$versionizedPriceId = $tceMain->getAutoVersionId(self::TABLE_Price, $placeholderPriceId);
+
+		/**
+		 * Placeholder (Live)
+		 */
+
+		$this->assertRecords(
+			array(
+				self::TABLE_Hotel => array(
+					$placeholderHotelId => array(
+						'pid' => self::VALUE_Pid,
+						't3ver_wsid' => 0,
+						't3ver_state' => 1,
+					),
+					$versionizedHotelId => array(
+						'pid' => -1,
+						't3ver_wsid' => self::VALUE_WorkspaceId,
+						't3ver_state' => -1,
+						self::FIELD_Hotel_Offers => $versionizedOfferId,
+					),
+				),
+				self::TABLE_Offer => array(
+					$placeholderOfferId => array(
+						'pid' => self::VALUE_Pid,
+						't3ver_wsid' => 0,
+					),
+				),
+				self::TABLE_Price => array(
+					$placeholderPriceId => array(
+						'pid' => self::VALUE_Pid,
+						't3ver_wsid' => 0,
+					),
+				),
+			)
+		);
+
+		/**
+		 * Workspace (Version)
+		 */
+
+		$this->assertChildren(
+			self::TABLE_Hotel, $versionizedHotelId, self::FIELD_Hotel_Offers,
+			array(
+				array(
+					'tableName' => self::TABLE_Offer,
+					'uid' => $versionizedOfferId,
+					'pid' => -1,
+					't3ver_id' => 1,
+					't3ver_oid' => $placeholderOfferId,
+				),
+			)
+		);
+
+		$this->assertChildren(
+			self::TABLE_Offer, $versionizedOfferId, self::FIELD_Offers_Prices,
+			array(
+				array(
+					'tableName' => self::TABLE_Price,
+					'uid' => $versionizedPriceId,
+					'pid' => -1,
+					't3ver_id' => 1,
+					't3ver_oid' => $placeholderPriceId,
+				),
+			)
+		);
+
+		$this->assertReferenceIndex(
+			array(
+				$this->combine(self::TABLE_Hotel, $versionizedHotelId, 'offers') => array(
+					$this->combine(self::TABLE_Offer, $versionizedOfferId),
+				),
+				$this->combine(self::TABLE_Offer , $versionizedOfferId, 'prices') => array(
+					$this->combine(self::TABLE_Price, $versionizedPriceId),
+				),
+			)
+		);
+	}
+
 	/**
 	 * @return void
 	 * @test
